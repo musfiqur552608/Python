62d2fbd9b850cd4ec5f9e2942a4edbd85b592b5c
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2022-02-26 09:51:58 +0600

    Logical operatiors

--- operators.py
+++ operators.py
@@ -16,7 +16,11 @@ print(30 <= 18)
 
 
 #Logical operators
-
+print(5 == 5 and 10 == 10)
+print(10 == 10 and 10 == 11)
+print(10 == 10 or 10 == 11)
+print(not True)
+print()
 
 #Assignment operators
 
