c8972a74bfc46464970d5f3d8e6944218252d451
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2022-02-26 08:58:11 +0600

    variables

---
+++ variable.py
@@ -0,0 +1,16 @@
+# variable declaratiuon and assignment
+name = "Musfiqur Rahman"
+num = 7
+age = 34
+
+#Multi-assignment within a single statement
+# name, num, age = "Ronaldo", 7, 34
+
+# output
+# print(name)
+# print(num)
+# print(age)
+
+#output multi values
+print(name, num, age)
+
