9beb6d0faeea989f3168b9d956a19cc8ea410fdf
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2022-02-26 09:25:27 +0600

    string_detection

---
+++ string_detection.py
@@ -0,0 +1,16 @@
+
+# String Detection
+
+x = "567hello"
+y = "101010"
+
+
+# Using isnumeric
+print(x.isnumeric()) #False
+print(y.isnumeric()) #True
+
+triple = """multi
+line
+string"""
+
+print(triple)
