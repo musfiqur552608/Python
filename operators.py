958f6d7ef788f9f222f5a952ab777586ab446c67
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2022-02-26 09:46:40 +0600

    Arithmetic operatiors

---
+++ operators.py
@@ -0,0 +1,17 @@
+# Arithmetic operatiors
+print(100 + 70)
+print(90 - 10)
+print(10 * 4)
+print(12 / 4)
+print(15 % 2)
+print(15 // 2)
+print(15 ** 2)
+
+#comparison operators
+
+
+#Logical operators
+
+#Assignment operators
+
+#Conditional operators
